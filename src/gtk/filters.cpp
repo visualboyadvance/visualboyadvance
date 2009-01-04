68e462350530c62835c23a914df66f8660a9a83f
bgk <bgk@a31d4220-a93d-0410-bf67-fe4944624d44>
2009-01-04 09:03:26 +0000

    Fixed build with asm scalers
    git-svn-id: https://vbam.svn.sourceforge.net/svnroot/vbam/trunk@845 a31d4220-a93d-0410-bf67-fe4944624d44

---
+++ src/gtk/filters.cpp
@@ -0,0 +1,104 @@
+// VisualBoyAdvance - Nintendo Gameboy/GameboyAdvance (TM) emulator.
+// Copyright (C) 1999-2003 Forgotten
+// Copyright (C) 2004 Forgotten and the VBA development team
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include "filters.h"
+
+void _2xSaI        (u8 *, u32, u8 *, u8 *, u32, int, int);
+void _2xSaI32      (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Super2xSaI    (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Super2xSaI32  (u8 *, u32, u8 *, u8 *, u32, int, int);
+void SuperEagle    (u8 *, u32, u8 *, u8 *, u32, int, int);
+void SuperEagle32  (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Pixelate      (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Pixelate32    (u8 *, u32, u8 *, u8 *, u32, int, int);
+void AdMame2x      (u8 *, u32, u8 *, u8 *, u32, int, int);
+void AdMame2x32    (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Bilinear      (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Bilinear32    (u8 *, u32, u8 *, u8 *, u32, int, int);
+void BilinearPlus  (u8 *, u32, u8 *, u8 *, u32, int, int);
+void BilinearPlus32(u8 *, u32, u8 *, u8 *, u32, int, int);
+void Scanlines     (u8 *, u32, u8 *, u8 *, u32, int, int);
+void Scanlines32   (u8 *, u32, u8 *, u8 *, u32, int, int);
+void ScanlinesTV   (u8 *, u32, u8 *, u8 *, u32, int, int);
+void ScanlinesTV32 (u8 *, u32, u8 *, u8 *, u32, int, int);
+void hq2x          (u8 *, u32, u8 *, u8 *, u32, int, int);
+void hq2x32        (u8 *, u32, u8 *, u8 *, u32, int, int);
+void lq2x          (u8 *, u32, u8 *, u8 *, u32, int, int);
+void lq2x32        (u8 *, u32, u8 *, u8 *, u32, int, int);
+
+void SmartIB       (u8 *, u32, int, int);
+void SmartIB32     (u8 *, u32, int, int);
+void MotionBlurIB  (u8 *, u32, int, int);
+void MotionBlurIB32(u8 *, u32, int, int);
+
+namespace VBA
+{
+
+struct {
+  char m_csName[30];
+  int m_iEnlargeFactor;
+  Filter m_apvFunc[2];
+}
+static const astFilters[] =
+{
+  { "None",                1, { NULL,         NULL           } },
+  { "2xSaI",               2, { _2xSaI,       _2xSaI32       } },
+  { "Super 2xSaI",         2, { Super2xSaI,   Super2xSaI32   } },
+  { "Super Eagle",         2, { SuperEagle,   SuperEagle32   } },
+  { "Pixelate",            2, { Pixelate,     Pixelate32     } },
+  { "AdvanceMAME Scale2x", 2, { AdMame2x,     AdMame2x32     } },
+  { "Bilinear",            2, { Bilinear,     Bilinear32     } },
+  { "Bilinear Plus",       2, { BilinearPlus, BilinearPlus32 } },
+  { "Scanlines",           2, { Scanlines,    Scanlines32    } },
+  { "TV Mode",             2, { ScanlinesTV,  ScanlinesTV32  } },
+  { "hq2x",                2, { hq2x,         hq2x32         } },
+  { "lq2x",                2, { lq2x,         lq2x32         } }
+};
+
+struct {
+  char m_csName[30];
+  FilterIB m_apvFunc[2];
+}
+static const astFiltersIB[] =
+{
+  { "None",                      { NULL,         NULL           } },
+  { "Smart interframe blending", { SmartIB,      SmartIB32      } },
+  { "Interframe motion blur",    { MotionBlurIB, MotionBlurIB32 } }
+};
+
+Filter pvGetFilter(EFilter _eFilter, EFilterDepth _eDepth)
+{
+  return astFilters[_eFilter].m_apvFunc[_eDepth];
+}
+
+char* pcsGetFilterName(const EFilter _eFilter)
+{
+        return (char*)astFilters[_eFilter].m_csName;
+}
+
+FilterIB pvGetFilterIB(EFilterIB _eFilterIB, EFilterDepth _eDepth)
+{
+  return astFiltersIB[_eFilterIB].m_apvFunc[_eDepth];
+}
+
+char* pcsGetFilterIBName(const EFilterIB _eFilterIB)
+{
+        return (char*)astFiltersIB[_eFilterIB].m_csName;
+}
+
+} // namespace VBA
---
+++ src/win32/Directories.cpp
@@ -0,0 +1,216 @@
+#include "stdafx.h"
+#include "vba.h"
+#include "Directories.h"
+#include "Reg.h"
+#include "WinResUtil.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// Directories dialog
+
+static int CALLBACK browseCallbackProc(HWND hWnd, UINT msg,
+                                       LPARAM l, LPARAM data)
+{
+  char *buffer = (char *)data;
+  switch(msg) {
+  case BFFM_INITIALIZED:
+    if(buffer[0])
+      SendMessage(hWnd, BFFM_SETSELECTION, TRUE, (LPARAM)buffer);
+    break;
+  default:
+    break;
+  }
+  return 0;
+}
+
+Directories::Directories(CWnd* pParent /*=NULL*/)
+  : CDialog(Directories::IDD, pParent)
+{
+}
+
+
+void Directories::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_SAVE_PATH, m_savePath);
+	DDX_Control(pDX, IDC_ROM_PATH, m_romPath);
+	DDX_Control(pDX, IDC_GBCROM_PATH, m_gbcromPath);
+	DDX_Control(pDX, IDC_GBROM_PATH, m_gbromPath);
+	DDX_Control(pDX, IDC_CAPTURE_PATH, m_capturePath);
+	DDX_Control(pDX, IDC_BATTERY_PATH, m_batteryPath);
+}
+
+
+BEGIN_MESSAGE_MAP(Directories, CDialog)
+  ON_BN_CLICKED(IDC_BATTERY_DIR, OnBatteryDir)
+  ON_BN_CLICKED(IDC_CAPTURE_DIR, OnCaptureDir)
+  ON_BN_CLICKED(IDC_GBROM_DIR, OnGbromDir)
+  ON_BN_CLICKED(IDC_ROM_DIR, OnRomDir)
+  ON_BN_CLICKED(IDC_SAVE_DIR, OnSaveDir)
+  ON_BN_CLICKED(IDC_GBCROM_DIR, OnGbcromDir)
+END_MESSAGE_MAP()
+
+  /////////////////////////////////////////////////////////////////////////////
+// Directories message handlers
+
+BOOL Directories::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+	CString p;
+
+	p = regQueryStringValue("romdir", NULL);
+	if(!p.IsEmpty())
+		GetDlgItem(IDC_ROM_PATH)->SetWindowText(p);
+
+	p = regQueryStringValue("gbcromdir", NULL);
+	if(!p.IsEmpty())
+		GetDlgItem(IDC_GBCROM_PATH)->SetWindowText(p);
+
+	p = regQueryStringValue("gbromdir", NULL);
+	if(!p.IsEmpty())
+		GetDlgItem(IDC_GBROM_PATH)->SetWindowText(p);
+
+	p = regQueryStringValue("batteryDir", DEFAULT_BATTERY_DIR);
+	if(!p.IsEmpty())
+		GetDlgItem(IDC_BATTERY_PATH)->SetWindowText( p);
+
+	p = regQueryStringValue("saveDir", DEFAULT_SAVESTATES_DIR);
+	if(!p.IsEmpty())
+		GetDlgItem(IDC_SAVE_PATH)->SetWindowText(p);
+
+	p = regQueryStringValue("captureDir", DEFAULT_SCREENSHOTS_DIR);
+	if(!p.IsEmpty())
+		GetDlgItem(IDC_CAPTURE_PATH)->SetWindowText(p);
+
+	return TRUE;
+	// return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void Directories::OnBatteryDir()
+{
+  m_batteryPath.GetWindowText(initialFolderDir);
+  CString p = browseForDir(winResLoadString(IDS_SELECT_BATTERY_DIR));
+  if(!p.IsEmpty())
+    m_batteryPath.SetWindowText(p);
+}
+
+void Directories::OnCaptureDir()
+{
+  m_capturePath.GetWindowText(initialFolderDir);
+  CString p = browseForDir(winResLoadString(IDS_SELECT_CAPTURE_DIR));
+  if(!p.IsEmpty())
+    m_capturePath.SetWindowText(p);
+}
+
+void Directories::OnGbromDir()
+{
+  m_gbromPath.GetWindowText(initialFolderDir);
+  CString p = browseForDir(winResLoadString(IDS_SELECT_ROM_DIR));
+  if(!p.IsEmpty())
+    m_gbromPath.SetWindowText(p);
+}
+
+void Directories::OnGbcromDir()
+{
+  m_gbcromPath.GetWindowText(initialFolderDir);
+  CString p = browseForDir(winResLoadString(IDS_SELECT_ROM_DIR));
+  if(!p.IsEmpty())
+    m_gbcromPath.SetWindowText(p);
+}
+
+void Directories::OnRomDir()
+{
+  m_romPath.GetWindowText(initialFolderDir);
+  CString p = browseForDir(winResLoadString(IDS_SELECT_ROM_DIR));
+  if(!p.IsEmpty())
+    m_romPath.SetWindowText(p);
+}
+
+void Directories::OnSaveDir()
+{
+  m_savePath.GetWindowText(initialFolderDir);
+  CString p = browseForDir(winResLoadString(IDS_SELECT_SAVE_DIR));
+  if(!p.IsEmpty())
+    m_savePath.SetWindowText(p);
+}
+
+void Directories::OnCancel()
+{
+  EndDialog(FALSE);
+}
+
+void Directories::OnOK()
+{
+	CDialog::OnOK();
+
+	CString buffer;
+
+	m_romPath.GetWindowText(buffer);
+	if( !buffer.IsEmpty() )
+		regSetStringValue( "romdir", buffer );
+	treatRelativePath( buffer );
+
+	m_gbcromPath.GetWindowText(buffer);
+	if( !buffer.IsEmpty() )
+		regSetStringValue( "gbcromdir", buffer );
+	treatRelativePath( buffer );
+
+	m_gbromPath.GetWindowText(buffer);
+	if( !buffer.IsEmpty() )
+		regSetStringValue( "gbromdir", buffer );
+	treatRelativePath( buffer );
+
+	m_batteryPath.GetWindowText(buffer);
+	if( !buffer.IsEmpty() )
+		regSetStringValue( "batteryDir", buffer );
+	treatRelativePath( buffer );
+
+	m_savePath.GetWindowText(buffer);
+	if( !buffer.IsEmpty() )
+		regSetStringValue( "saveDir", buffer );
+	treatRelativePath( buffer );
+
+	m_capturePath.GetWindowText(buffer);
+	if( !buffer.IsEmpty() )
+		regSetStringValue( "captureDir", buffer );
+	treatRelativePath( buffer );
+
+	EndDialog(TRUE);
+}
+
+CString Directories::browseForDir(CString title)
+{
+  static char buffer[1024];
+  LPMALLOC pMalloc;
+  LPITEMIDLIST pidl;
+
+  CString res;
+
+  if(SUCCEEDED(SHGetMalloc(&pMalloc))) {
+    BROWSEINFO bi;
+    ZeroMemory(&bi, sizeof(bi));
+    bi.hwndOwner = m_hWnd;
+    bi.lpszTitle = title;
+    bi.pidlRoot = 0;
+    bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_USENEWUI;
+    bi.lpfn = browseCallbackProc;
+    bi.lParam = (LPARAM)(LPCTSTR)initialFolderDir;
+
+    pidl = SHBrowseForFolder(&bi);
+
+    if(pidl) {
+      if(SHGetPathFromIDList(pidl, buffer)) {
+        res = buffer;
+      }
+      pMalloc->Free(pidl);
+      pMalloc->Release();
+    }
+  }
+  return res;
+}
