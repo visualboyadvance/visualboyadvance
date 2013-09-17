4daf9c05fd53d87adbac60831b7d7e20d597127c
Bastien Bouclet <bastien.bouclet@gmail.com>
2013-09-17 19:55:15 +0200

    GBA: Globals to C

--- src/gba/Globals.h
+++ src/gba/Globals.h
@@ -1,8 +1,31 @@
-#ifndef GLOBALS_H
-#define GLOBALS_H
+// VisualBoyAdvance - Nintendo Gameboy/GameboyAdvance (TM) emulator.
+// Copyright (C) 1999-2003 Forgotten
+// Copyright (C) 2005-2006 Forgotten and the VBA development team
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#ifndef VBAM_GBA_GLOBALS_H_
+#define VBAM_GBA_GLOBALS_H_
 
 #include <glib.h>
 #include "../common/Types.h"
+
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern gboolean speedup;
 extern int layerEnable;
@@ -103,4 +126,9 @@ extern u16 IME;
 # define UNLIKELY(x) (x)
 #endif
 
-#endif // GLOBALS_H
+/* Ends C function definitions when using C++ */
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VBAM_GBA_GLOBALS_H_
